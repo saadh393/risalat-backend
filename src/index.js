2cb287bcfe1b07881f3fcde1ca4467d200a53cc0
Saad Hasan <saadh393@gmail.com>
2022-10-11 23:11:07 +0600

    Deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
    * An asynchronous register function that runs before
    * your application is initialized.
-   *
    * This gives you an opportunity to extend code.
    */
   register(/*{ strapi }*/) {},
